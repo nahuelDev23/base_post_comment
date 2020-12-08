166b307db8501098c08f9aeb21b270ec71055593
Agony <agony@localhost.localdomain>
2020-12-08 14:44:42 -0300

    instale  composer pusher y descomente broadcastserviceprovider de app.php

--- public/js/app.js
+++ public/js/app.js
@@ -63331,8 +63331,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /opt/lampp/htdocs/notificaciones/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /opt/lampp/htdocs/notificaciones/resources/css/app.css */"./resources/css/app.css");
+__webpack_require__(/*! /opt/lampp/htdocs/base_post_comment/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /opt/lampp/htdocs/base_post_comment/resources/css/app.css */"./resources/css/app.css");
 
 
 /***/ })
